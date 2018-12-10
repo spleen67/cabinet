fd9de1970f8e9ca5118a1d792b6ea74fe1e9d6e4
Eric Berthomier <berthomier.eric@gmail.com>
2018-12-10 23:11:19 +0100

    modification docs_controller et routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+
+  resources :docs #creation de toutes les ressources, create, delete, update dans routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
