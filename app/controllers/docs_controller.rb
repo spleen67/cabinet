eed2e1fe363b94ebf2488b3e0ef15555ae46660c
Eric Berthomier <berthomier.eric@gmail.com>
2018-12-13 23:30:41 +0100

    edition, suppression, modification d un livre

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,32 +1,51 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy] # executer une methode action avant les actions show, edit update
 
   def index
+    #@doc = Doc.all.order("created_at DESC")
+    @docs = Doc.all
   end
 
   def show
   end
 
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc =  Doc.create(doc_params)
+   if @doc.save
+    redirect_to @doc
+   else
+    render :new
+   end
   end
 
   def edit
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+     else
+      render :edit
+    end
   end
 
-  def destroyed
+  def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
   def find_doc
+    @doc = Doc.find(params[:id])
   end
 
   def doc_params
+    params.require(:doc).permit(:title, :content)
   end
-  
+
 end
