75020f8caff1ad15c8883f603c6a4cb6714685d6
Eric Berthomier <berthomier.eric@gmail.com>
2018-12-14 21:54:31 +0100

    home page different pour les users authentifié

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :docs #creation de toutes les ressources, create, delete, update dans routes
   get 'welcome/index'
+
+  authenticated :user do
+    root "docs#index",  as: "authenticated_root" #pour les user authentifiés, le root est docs#index
+  end
 
   root 'welcome#index'
 
-  resources :docs #creation de toutes les ressources, create, delete, update dans routes
+ 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
